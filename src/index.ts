d4fe626e061ec5c8739be89a5a5b5495a5270efe
Ranjeet Gautam <gautamr@gautamr-a02.vmware.com>
2022-03-06 08:14:21 +0000

    Align command inlign with cht

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
-import {calculateRamdom} from './calculate';
+import { calculateRamdom } from "./calculate";
 
 (() => {
   console.log("Hello World");
   const root: HTMLElement = document.getElementById("root")!;
   const header = document.createElement("h1");
-  const text = document.createTextNode("Hello World : Random no - " + calculateRamdom());
+  const text = document.createTextNode(
+    "Hello World : Random no - " + calculateRamdom()
+  );
   header.appendChild(text);
   root.appendChild(header);
   console.log(root);
