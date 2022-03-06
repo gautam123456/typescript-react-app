fba4f9cccdb732617f62f34133c0279fdcbf22dd
Gautam <rkgautam2004@gmail.com>
2022-03-06 17:03:14 +0000

    Testing husky integration

--- husky.js
+++ husky.js
@@ -1,5 +1,5 @@
 export default {
   hooks: {
-    'pre-commit': 'pretty-quick --staged',
+    "pre-commit": "pretty-quick --staged",
   },
 };
--- src/calculate.ts
+++ src/calculate.ts
@@ -1,3 +1,3 @@
-export function calculateRamdom(){
+export function calculateRamdom() {
   return Math.random();
 }
